78187345c7e9d4153fb1edcb8f08b112bbeabf49
Eugene <chepurko0@gmail.com>
2019-02-06 16:25:28 +0200

    Decoding of Caesar's cipher

--- StringCoder/MainWindow.xaml.cs
+++ StringCoder/MainWindow.xaml.cs
@@ -18,29 +18,34 @@ namespace StringCoder
     public partial class MainWindow : Window
     {
         readonly char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        readonly char[] alphabetUpper = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        int key = 1;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
 
         private void EncodeText(object sender, RoutedEventArgs e)
         {
-            //YourText.Text.Replace(" ", null);
             if (EncodedText.Text != "")
             {
                 EncodedText.Clear();
             }
 
-            int key = 15;
             try
             {
-                char[] text = YourText.Text.ToCharArray().Where(s => !Char.IsWhiteSpace(s)).ToArray();
+                char[] text = YourText.Text.ToCharArray().Where(s => !char.IsWhiteSpace(s)).ToArray();
                 for (int i = 0; i < text.Length; i++)
                 {
-                    for (int j = 0; j <= alphabet.Length; j++)
+                    for (int j = 0; j <= alphabet.Length && j <= alphabetUpper.Length; j++)
                     {
-                        if (text[i].ToString() == alphabet[j].ToString())
+                        if (text[i].ToString() == alphabet[j].ToString() || text[i].ToString() == alphabetUpper[j].ToString())
                         {
+                            char.ToLower(text[i]);
+
                             text[i] = Convert.ToChar(alphabet[(j + key) % alphabet.Length]);
                             EncodedText.Text += text[i].ToString();
                             break;
@@ -57,7 +62,31 @@ namespace StringCoder
 
         private void DecodeText(object sender, RoutedEventArgs e)
         {
-
+            if (DecodedText.Text != "")
+            {
+                DecodedText.Clear();
+            }
+            try
+            {
+                char[] text = EncodedText.Text.ToCharArray();
+                for (int i = 0; i < text.Length; i++)
+                {
+                    for (int j = 0; j <= alphabet.Length && j <= alphabetUpper.Length; j++)
+                    {
+                        if (text[i].ToString() == alphabet[j].ToString())
+                        {
+                            text[i] = Convert.ToChar(alphabet[(j - key) % alphabet.Length]);
+                            DecodedText.Text += text[i].ToString();
+                            break;
+                        }
+                        continue;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
