26ac1c7d01f6921d99d35a025ea8ac1e05380f9a
Eugene <chepurko0@gmail.com>
2019-02-08 17:43:45 +0200

    Decoding & removing symbols is ready

--- StringCoder/MainWindow.xaml.cs
+++ StringCoder/MainWindow.xaml.cs
@@ -19,13 +19,12 @@ namespace StringCoder
     {
         readonly char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         readonly char[] alphabetUpper = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        readonly string[] charsToRemove = new string []{ ",", ".", ";", ":", "@", "!", "#" };
         int key = 1;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            
         }
 
         private void EncodeText(object sender, RoutedEventArgs e)
@@ -37,13 +36,23 @@ namespace StringCoder
 
             try
             {
+                foreach (var item in charsToRemove)
+                {
+                    YourText.Text = YourText.Text.Replace(item, string.Empty);
+                }
+                
                 char[] text = YourText.Text.ToCharArray().Where(s => !char.IsWhiteSpace(s)).ToArray();
+
                 for (int i = 0; i < text.Length; i++)
                 {
                     for (int j = 0; j <= alphabet.Length && j <= alphabetUpper.Length; j++)
                     {
                         if (text[i].ToString() == alphabet[j].ToString() || text[i].ToString() == alphabetUpper[j].ToString())
                         {
+
+                            //text[i] = Convert.ToChar(text[i].ToString().Replace(text[i], Convert.ToChar("")));
+                            
+                            //charsToRemove[i].ToString().Replace(Convert.ToChar(text[i]), Convert.ToChar(""));
                             char.ToLower(text[i]);
 
                             text[i] = Convert.ToChar(alphabet[(j + key) % alphabet.Length]);
@@ -68,15 +77,23 @@ namespace StringCoder
             }
             try
             {
+                int temp = 0;
                 char[] text = EncodedText.Text.ToCharArray();
                 for (int i = 0; i < text.Length; i++)
                 {
-                    for (int j = 0; j <= alphabet.Length && j <= alphabetUpper.Length; j++)
+                    for (int j = 0; j <= alphabet.Length; j++)
                     {
                         if (text[i].ToString() == alphabet[j].ToString())
                         {
-                            text[i] = Convert.ToChar(alphabet[(j - key) % alphabet.Length]);
-                            DecodedText.Text += text[i].ToString();
+                            temp = j - key;
+
+                            if (temp < 0)
+                                temp += alphabet.Length;
+                            if (temp >= alphabet.Length)
+                                temp -= alphabet.Length;
+
+                            text[i] = alphabet[temp];
+                            DecodedText.Text += text[i];
                             break;
                         }
                         continue;
